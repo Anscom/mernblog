611776571ed9d5157a71b417948faffcf2eaf43e
Anscom <ooicheongwen123456@gmail.com>
2024-02-24 20:26:17 +0800

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
 import authRoute from './routes/auth.route.js';
+import postRoutes from './routes/post.routes.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -31,6 +32,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoute);
+app.use('/api/post', postRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
